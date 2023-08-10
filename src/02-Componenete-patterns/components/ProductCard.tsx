de3e06eaf11379cfb5de8131ae0eeb930c4b2b53
alberto8812 <cvelascosaavedra@gmail.com>
2023-08-09 19:47:21 -0500

    inicial compount params

--- src/02-Componenete-patterns/components/ProductCard.tsx
+++ src/02-Componenete-patterns/components/ProductCard.tsx
@@ -15,24 +15,50 @@ interface Props{
     Product: Product;
 }
 
+export const ProductImage=({img=''})=>{
+ return (
+  <img src={img?img:noImage} alt='producto imgr' className={styles.productImg} />
+ )
+}
+
+
+export const ProductoTitle=({title}:{title:string})=>{
+
+  return(
+      <span className={styles.productDescription}>{title}</span>
+  )
+}
+
+interface ProductButtonsProps{
+  Counter:number;
+  increaseBy:(value:number) => void;
+}
+
+export const ProductButtons=({Counter,increaseBy}:ProductButtonsProps)=>{
+ 
+  return(
+    
+  
+    <div className={styles.buttonsContainer}>
+        <button className={styles.buttonMinus} onClick={()=>increaseBy(-1)}>-</button>
+
+        <div className={styles.countLabel}>{Counter}</div>
+
+        <button className={styles.buttonAdd} onClick={()=>increaseBy(1)}>+</button>
+    </div>
+  )
+}
+
 
 export const ProductCard = ({Product}:Props) => {
+  const {Counter,increaseBy}=UseProduct()
 
-const {Counter,increaseBy}=UseProduct()
   return (
     <div className={styles.productCard}>
-      <img src={Product.img?Product.img:noImage} alt={noImage} className={styles.productImg} />
-
-
-        <span className={styles.productDescription}>{Product.title}</span>
-
-        <div className={styles.buttonsContainer}>
-            <button className={styles.buttonMinus} onClick={()=>increaseBy(-1)}>-</button>
-
-            <div className={styles.countLabel}>{Counter}</div>
-
-            <button className={styles.buttonAdd} onClick={()=>increaseBy(1)}>+</button>
-        </div>
+      
+      <ProductImage img={Product.img}/>
+      <ProductoTitle title={Product.title}/>
+      <ProductButtons Counter={Counter} increaseBy={increaseBy}/>
     </div>
 
     
