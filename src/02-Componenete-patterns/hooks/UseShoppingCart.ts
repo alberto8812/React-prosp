44d5479eca0f52cd360f659e4e61955ba112e306
alberto8812 <cvelascosaavedra@gmail.com>
2023-08-14 22:27:39 -0500

    producto cart

---
+++ src/02-Componenete-patterns/hooks/UseShoppingCart.ts
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import { onChangeArgs } from '../interface/interfaces';
+import { ProductInCart } from '../page/ShoppingPage';
+
+
+export const UseShoppingCart = () => {
+
+    const [shoppingCart, setShoppingCart] = useState<{[key:string]:ProductInCart}>({});
+
+    const onproductCountChange = ({Count,Product}:onChangeArgs)=>{
+
+      setShoppingCart(oldShoppingCart=>{
+        const prodcutIncart:ProductInCart=oldShoppingCart[Product.id] || {...Product,count:0}//SI EL OBJETO NO EXISTE LO CARGA CON 0
+        
+        if(Math.max(prodcutIncart.count + Count,0)>0){// EVALUA SI E MAYOR A 0
+          prodcutIncart.count = Count + 1;
+          return{
+            ...oldShoppingCart,
+            [Product.id]: prodcutIncart
+          }
+        }
+        const{[Product.id]:toDelete,...rest}=oldShoppingCart;//desetructuramos el elemento que quiero el minar el numero de id.
+        return rest;//
+
+      })
+  
+    };
+
+
+
+
+    
+  return{
+    onproductCountChange,
+    shoppingCart
+  }
+}
