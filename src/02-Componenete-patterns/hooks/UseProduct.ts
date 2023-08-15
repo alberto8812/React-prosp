1fb449fe121f79a894ea10f75fc3a8f621630261
alberto8812 <cvelascosaavedra@gmail.com>
2023-08-14 20:48:07 -0500

    sync value buycar

--- src/02-Componenete-patterns/hooks/UseProduct.ts
+++ src/02-Componenete-patterns/hooks/UseProduct.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Product, onChangeArgs } from "../interface/interfaces";
 
 interface Props{
@@ -9,15 +9,21 @@ interface Props{
 
 export const UseProduct = ({onChange,Product,Count=0}:Props) => {
 
-    const [Counter, setCounter] = useState(Count);
-
+    const [Counter, setCounter] = useState(Count);//count viene con un valor inicial, si cpunt llega con un valor el usesteate no vuelve a cambiar 
+ 
 
     const increaseBy=(value:number)=>{
         const newValue=Math.max(Counter+value,0)
         setCounter(newValue)
         onChange && onChange({Count:newValue,Product});
+     
     };
 
+//actualiza  el setCounter con eñ nuveo valor
+    useEffect(() => {
+      setCounter(Count);
+    
+    }, [Count])
 
 
   return {Counter,increaseBy}
