e6bbd9474ee218551a6abeb49bb851473be85e9a
alberto8812 <cvelascosaavedra@gmail.com>
2023-08-21 15:48:39 -0500

    commun form

--- src/Routes/Navegation.tsx
+++ src/Routes/Navegation.tsx
@@ -4,6 +4,7 @@ import { Routes,Route,NavLink } from 'react-router-dom'
 import logo from "../logo.svg"
 
 import { ShoppingPage } from "../02-Componenete-patterns/page/ShoppingPage";
+import { RegisterPage } from "../03.forms/pages/RegisterPage";
 
 
 
@@ -18,6 +19,7 @@ export const Navegation = () => {
                     <img src={logo} alt="logo" />
                     <ul>
                      <li key="shopping"><NavLink to="/" className={({isActive})=>isActive?'nac-active':''}>Shopping page</NavLink></li>
+                     <li key="form"><NavLink to="/Register" className={({isActive})=>isActive?'nac-active':''}>Registes Page</NavLink></li>
                        
 
                     </ul>
@@ -25,6 +27,7 @@ export const Navegation = () => {
 
                 <Routes>
                 <Route path="/" element={<ShoppingPage/>} key="shopping"/>
+                <Route path="/Register" element={<RegisterPage/>} key="shopping"/>
                     {/*routes.map(({path,Component})=>(<Route path={path} element={<Component/>} key={path}/>))*/}
                     
 
